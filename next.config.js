b525038c6166f4e3b698815d7cfa34685559b032
Waseem Akram <waseem.akram@ranktri.com>
2026-02-22 17:45:18 +0530

    Remove template code and fix lockfiles for Vercel deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,1 @@
-const nextra = require('nextra')
-
-const withNextra = (nextra.default || nextra)({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
-
-module.exports = withNextra()
+module.exports = {}
